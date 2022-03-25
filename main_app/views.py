5391ce9a84c49d507788b55eaaa870524dc53bd9
My Mac <home@Js-MacBook-Pro.local>
2022-03-25 16:26:51 -0400

    django0325

---
+++ main_app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Add the following import
+from django.http import HttpResponse
+
+
+class Finch:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+finchs = [
+  Finch('Birdie', 'blue', 'always biting', 3),
+  Finch('Flyee', 'red', 'large beak', 0),
+  Finch('Raven', 'black', '3 legged finch', 4)
+]
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello Birds!</h1>')
+
+# def about(request):
+#   return HttpResponse('<h1>About /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finchs_index(request):
+  return render(request, 'finchs/index.html', { 'finchs': finchs })
+
+
