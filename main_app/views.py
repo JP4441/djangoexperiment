226d5201e707078deca97dc7ceb8fa1a0303e54b
My Mac <home@Js-MacBook-Pro.local>
2022-03-28 14:25:45 -0400

    03281425

--- main_app/views.py
+++ main_app/views.py
@@ -2,25 +2,25 @@ from django.shortcuts import render
 
 # Add the following import
 from django.http import HttpResponse
+from .models import Finch
 
 
-class Finch:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
+# class Finch:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, breed, description, age):
+#     self.name = name
+#     self.breed = breed
+#     self.description = description
+#     self.age = age
 
-finchs = [
-  Finch('Birdie', 'blue', 'always biting', 3),
-  Finch('Flyee', 'red', 'large beak', 0),
-  Finch('Raven', 'black', '3 legged finch', 4)
-]
+# finchs = [
+#   Finch('Birdie', 'blue', 'always biting', 3),
+#   Finch('Flyee', 'red', 'large beak', 0),
+#   Finch('Raven', 'black', '3 legged finch', 4)
+# ]
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello Birds!</h1>')
-
+  return render(request, 'home.html')  
 # def about(request):
 #   return HttpResponse('<h1>About /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
@@ -28,6 +28,7 @@ def about(request):
   return render(request, 'about.html')
 
 def finchs_index(request):
+  finchs = Finch.objects.all()
   return render(request, 'finchs/index.html', { 'finchs': finchs })
 
 
