98d29f613bddb901f47966f76e74d91444134d56
My Mac <home@Js-MacBook-Pro.local>
2022-03-29 14:57:38 -0400

    03291457

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,12 @@ from django.urls import reverse
 
 # Create your models here.
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
 
 class Finch(models.Model):
   name = models.CharField(max_length=100)
@@ -15,3 +21,14 @@ class Finch(models.Model):
 ## redirecting user to details after uploading new finch profile
   def get_absolute_url(self):
       return reverse('detail', kwargs={'finch_id': self.id})
+
+  def __str__(self):
+    return self.name
+
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
